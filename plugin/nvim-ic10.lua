a0d6ece4f0bc06c612da3e381bcd71402dcfa52d
Sascha Krüger <sascha@skhome.me>
2025-10-17 13:51:24 +0200

    Fix revision

--- plugin/nvim-ic10.lua
+++ plugin/nvim-ic10.lua
@@ -4,7 +4,6 @@ vim.api.nvim_create_autocmd("User", {
 		require("nvim-treesitter.parsers").ic10 = {
 			install_info = {
 				url = "https://github.com/skhome/tree-sitter-ic10",
-				revision = "HEAD",
 			},
 		}
 	end,
